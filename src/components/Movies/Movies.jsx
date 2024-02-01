69916b8540f76efb92b86da9f5f04bb48542b6fc
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2024-02-01 02:03:23 +0200

    ....

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,35 @@
+import style from './movies.module.css';
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import FormSearchMovie from 'components/FormSearchMovie/FormSearchMovie';
+
+export const Movies = () => {
+  const [itemMovie, setItemMovie] = useState([]);
+
+  const renderMoviList = () => {
+    return itemMovie.map(({ title, id }) => {
+      return (
+        <li className={style.li} key={id}>
+          <NavLink className={style.link} to={`${id}`}>
+            {title}
+          </NavLink>
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div className={style.div}>
+      <FormSearchMovie setItemMovie={setItemMovie} />
+      <ul className={style.list}>
+        {itemMovie.length ? (
+          renderMoviList()
+        ) : (
+          <p className={style.p}>
+            Please enter the title of the movie to search
+          </p>
+        )}
+      </ul>
+    </div>
+  );
+};
