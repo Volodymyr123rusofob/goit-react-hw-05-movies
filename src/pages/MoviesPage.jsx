bf2307f363f56309ed410948b874532415719cf5
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2024-02-03 00:10:35 +0200

    bag-fix_url1

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,4 +1,4 @@
-import { FormSearchMovie } from 'components/Movies/FormSearchMovie/FormSearchMovie';
+import { FormSearchMovie } from 'components/FormSearchMovie/FormSearchMovie';
 
 const MoviesPage = () => {
   return (
